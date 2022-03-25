4621dc71417e5eca43321dafc4dbb91bb98c5dd7
KamrynNJ <51929727+KamrynNJ@users.noreply.github.com>
2022-03-25 16:01:13 -0400

    product display and data entity works, saleperson display and data entity works

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class Salesperson(ndb.Model):
   first_name_s = ndb.StringProperty(required=True)
   last_name_s = ndb.StringProperty(required=True)
   address_s= ndb.StringProperty(required=True)
-  phone_s=ndb.FloatProperty(required=True)
+  phone_s=ndb.StringProperty(required=True)
   start_date_s=ndb.StringProperty(required=True)
   end_date_s=ndb.StringProperty(required=True)
   manager_s=ndb.StringProperty(required=True)
@@ -65,6 +65,16 @@ class DisplayProductPage(webapp2.RequestHandler):
         products_all=Products.query().order(Products.name_of_product).fetch()
         self.response.write(displayProduct_template.render({'product_info': products_all,
                                                     }))  # the response
+class AddSalespersonPage(webapp2.RequestHandler):
+    def get(self):  # for a get request
+        addSalesperson_template = the_jinja_env.get_template('html/addSalesperson.html')
+        self.response.write(addSalesperson_template.render())  # the response
+class DisplaySalespersonPage(webapp2.RequestHandler):
+    def get(self):  # for a get request
+        displaySalesperson_template = the_jinja_env.get_template('html/salespersonDisplay.html')
+        salesperson_all=Salesperson.query().order(Salesperson.first_name_s).fetch()
+        self.response.write(displaySalesperson_template.render({'salesperson_info': salesperson_all,
+                                                    }))  # the response
 class ShowProduct(webapp2.RequestHandler):
     def post(self):
         results_product_template = the_jinja_env.get_template('html/addProductConfirm.html')
@@ -92,6 +102,32 @@ class ShowProduct(webapp2.RequestHandler):
         # pass that dictionary to the Jinja2 `.render()` method
         self.response.write(results_product_template.render(the_variable_product_dict))
 
+class ShowSalesperson(webapp2.RequestHandler):
+    def post(self):
+        results_salesperson_template = the_jinja_env.get_template('html/addSalespersonConfirm.html')
+        # Access the user data via the form's input elements' names.
+        salesperson_first_name = self.request.get('firstNameGiven')
+        salesperson_last_name = self.request.get('lastNameGiven')
+        salesperson_address=self.request.get('addressGiven')
+        salesperson_phone=self.request.get('phoneGiven')
+        salesperson_start_date=self.request.get('StartDateGiven')
+        salesperson_end_date=self.request.get('EndDateGiven')
+        salesperson_manager=self.request.get('managerGiven')
+        # Organize that user data into a dictionary.
+        the_variable_salesperson_dict = {
+            "first_name_from_form": salesperson_first_name,
+            "last_name_from_form": salesperson_last_name,
+            "address_from_form": salesperson_address,
+            "phone_from_form":salesperson_phone,
+            "start_date_from_form":salesperson_start_date,
+            "end_date_from_form":salesperson_end_date,
+            "manager_from_form":salesperson_manager,
+        }
+        new_salesperson_template=Salesperson(first_name_s=salesperson_first_name, last_name_s=salesperson_last_name, address_s=salesperson_address, phone_s=salesperson_phone, start_date_s=salesperson_start_date, end_date_s=salesperson_end_date, manager_s=salesperson_manager)
+        new_salesperson_template.put()
+        print(new_salesperson_template)
+        # pass that dictionary to the Jinja2 `.render()` method
+        self.response.write(results_salesperson_template.render(the_variable_salesperson_dict))
 
 # the app configuration section
 app = webapp2.WSGIApplication([
@@ -99,4 +135,7 @@ app = webapp2.WSGIApplication([
     ('/displayProduct', DisplayProductPage),
     ('/addProduct', AddProductPage),
     ('/addProductConfirm', ShowProduct),
+    ('/displaySalesperson', DisplaySalespersonPage),
+    ('/addSalesperson', AddSalespersonPage),
+    ('/addSalespersonConfirm', ShowSalesperson),
 ], debug=True)
